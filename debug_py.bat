3131b0988908e83ca25957d1c3d8c3cdc987fb8b
Nathaniel Berens <nrberens@gmail.com>
2013-08-11 22:55:10 -0400

    Uploading existing unversioned source

---
+++ debug_py.bat
@@ -0,0 +1,10 @@
+@echo off
+
+cd C:\Users\Nathaniel\Dropbox\purge\
+orbital.py
+
+if not errorlevel 1 goto quit
+echo.
+echo.
+pause
+:quit
---
+++ python_wrap.bat
@@ -0,0 +1,2 @@
+C:\Python26\python.exe %1
+IF %ERRORLEVEL% NEQ 0 PAUSE
